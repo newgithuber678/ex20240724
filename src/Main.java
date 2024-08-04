09dfb05452feba9c61c902e84e9c7ff4280102fa
intelcore <becar_02@mail.ru>
2024-08-04 22:00:36 +0300

    ex20240724

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            int[] nums={0,1,2,3};
+            for (int i=0;i<10;i++){
+                nums[i]++;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Ошибка");
+        }
     }
 }
